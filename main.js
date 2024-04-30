065e86474025296adb18129a226395c70a0a3a8c
moulichippada <142070388+moulichippada@users.noreply.github.com>
2024-04-30 11:29:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+    strings: ["Front End Developer", "Competative Programmer", "SQL Developer", "Data Analyst"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
+function openCertificate(certificate) {
+    window.open(certificate);
+}
+function opentab(certificate) {
+    window.open(certificate);
+}
